b0edc2a2a0ca55586713b8ea6bd52932c3a69ea6
d1kolyadin <84541710+d1kolyadin@users.noreply.github.com>
2021-06-09 23:18:03 +0300

    chip_seeker added

---
+++ src/chip_seeker.R
@@ -0,0 +1,24 @@
+setwd("C:\\local files\\bi\\project\\src")
+source('lib.R')
+
+###
+
+#NAME <- 'H3K4me3_A549.ENCFF046DTX.hg19.filtered'
+NAME <- 'H3K4me3_A549.ENCFF922TPS.hg19.filtered'
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
+plotAnnoPie(peakAnno)
+dev.off()
+
+# peak <- readPeakFile(BED_FN)
+# pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+# covplot(peak, weightCol="V5")
+# dev.off()
+# 
